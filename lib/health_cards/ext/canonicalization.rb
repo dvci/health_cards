1bd38940cd3e551d5431f2bdd4176ef8dd9c256e
Reece Adamson <radamson@mitre.org>
2020-12-21 15:48:00 -0500

    add ext

---
+++ lib/health_cards/ext/canonicalization.rb
@@ -0,0 +1,15 @@
+module HealthCards
+  module Canonicalization
+    def to_json_c14n
+      puts 'ok'
+      "{" + self.
+        keys.
+        sort_by {|k| k.to_s.encode(Encoding::UTF_16)}.
+        map {|k| k.to_s.to_json_c14n + ':' + self[k].to_json_c14n}
+                .join(',') +
+        '}'
+    end
+  end
+end
+
+
