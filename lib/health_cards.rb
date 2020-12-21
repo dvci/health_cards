f244a0940b882e2a42bd95883dbc97b16b46a916
Reece Adamson <radamson@mitre.org>
2020-12-21 15:48:00 -0500

    add ext

--- lib/health_cards.rb
+++ lib/health_cards.rb
@@ -3,8 +3,14 @@
 require 'health_cards/version'
 require 'health_cards/verifiable_credential'
 require 'json/canonicalization'
+require_relative 'health_cards/ext/canonicalization'
+
+Hash.prepend HealthCards::Canonicalization
 
 module HealthCards
   class Error < StandardError; end
   # Your code goes here...
 end
+
+data = {a: 'a', b: 'b'}
+puts data.to_json_c14n
