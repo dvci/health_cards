d1308c0cca9a8c4c3a5c8d024cfe0202b7b58cba
pmadria <pmadria@mitre.org>
2021-02-15 09:41:49 -0600

    added fetch capabilities in patient_form

--- forms/app.rb
+++ forms/app.rb
@@ -2,17 +2,52 @@
 
 require 'sinatra'
 require 'json'
+require 'fhir_client'
+require 'fhir_models'
+require 'uri'
 
 set :port, 8080
 set :static, true
 set :public_folder, 'static'
 set :views, 'views'
 
+# client = FHIR::Client.new(localhost:8080)
+# FHIR::Model.client = client
+# client.default_json
+
+# before do
+#   content_type :application/fhir+json
+# end
+
+
 get '/' do
   erb :patient_form
 end
 
-post '/Patient/' do
+post '/Patient/', :provides => :json do
+  #data will be in the body and not params when I change it into application/json
+  # request.body.rewind
+  # data_payload = JSON.parse request.body.read
+  # puts data_payload
+
+  # patient = { given: data_payload['given'], 
+  #             mi: data_payload['mi'], 
+  #             ln: data_payload['ln'], 
+  #             suffix: data_payload['suffix'],
+  #             gender: data_payload['gender'],
+  #             telecom: data_payload['telecom'],
+  #             email: data_payload['email']
+  #             birthdate: data_payload['birthdate'] }
+
+  # given = data_payload['given']
+  # mi = data_payload['mi']
+  # ln = data_payload['ln']
+  # suffix = data_payload['suffix']
+  # gender = data_payload['gender']
+  # telecom = data_payload['telecom']
+  # email = data_payload['email']
+  # birthdate = data_payload['birthdate']
+
   given = params['given']
   mi = params['mi']
   ln = params['ln']
@@ -28,7 +63,7 @@ post '/Patient/' do
 end
 
 get '/Patient' do
-  content_type :json
+  #content_type :application/fhir+json
   # gender = { "gender" => params['gender'] }
   # gender.to_json
   patient = { 'given' => params['given'], 'mi' => params['mi'], 'ln' => params['ln'], 'suffix' => params['suffix'],
@@ -36,3 +71,17 @@ get '/Patient' do
               'email' => params['email'], 'birthdate' => params['birthdate'] }
   patient.to_json
 end
+
+
+# reply = client.read_feed(FHIR::Patient) # fetch Bundle of Patients
+# bundle = reply.resource
+# bundle.entry.each do |entry|
+#   patient = entry.resource
+#   puts patient.name[0].text
+# end
+# puts reply.code # HTTP 200 (or whatever was returned)
+# puts reply.body # Raw XML or JSON
+
+
+#fix frontend to send the data correctly - submit as a request body and valid fhir resource 
+#take it from the backend and make sure it is a fhir resource
