13b2105e9cb521a983b6948c76260d1dce478253
pmadria <pmadria@mitre.org>
2021-02-16 15:27:14 -0600

    still working

--- frontend/src/PatientForm.js
+++ frontend/src/PatientForm.js
@@ -2,22 +2,35 @@ import React from "react";
 import { useState } from "react";
 
 function PatientForm() {
-  const [name, setName] = useState("Bob");
+
+  const [name, setName] = useState('First Name');
+  const [gender, setGender] = useState('Gender');
+  const [phone, setPhone] = useState('Phone');
+  const [email, setEmail] = useState('Email');
+  const [birthDate, setBirthDate] = useState('Date of Birth');
+
+
+  function handleSubmit() {
+    console.log('woo')
+  }
 
   function submitData() {
-    fetch("https://webhook.site/654fb84b-082a-41aa-87e5-f05a5a62b1bb", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      mode: "no-cors",
-      body: JSON.stringify({ resourceType: "Patient", name: name }),
-    });
+    fetch("http://localhost:3000/Patient/",
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        mode: 'no-cors',
+        body: JSON.stringify({resourceType: 'Patient', name: name})
+      }
+      )
   }
 
   function handleChange(event) {
     setName(event.target.value);
     console.log(name);
+    console.log(gender)
   }
 
   return (
@@ -25,6 +38,27 @@ function PatientForm() {
       <label>
         First Name:
         <input type="text" value={name} onChange={handleChange} />
+      </label>
+      <label>Middle Initial:
+        <input type="text" value={name} onChange={handleChange} />
+      </label>
+      <label>Last Name:
+        <input type="text" value={name} onChange={handleChange} />
+      </label>
+      <label>Suffix:
+        <input type="text" value={name} onChange={handleChange} />
+      </label>
+      <label>Gender:
+        <input type="text" value={gender} onChange={handleChange} />
+      </label>
+      <label>Mobile Phone:
+        <input type="text" value={phone} onChange={handleChange} />
+      </label>
+      <label>Email Address:
+        <input type="text" value={email} onChange={handleChange} />
+      </label>
+      <label>Date of Birth:
+        <input type="text" value={birthDate} onChange={handleChange} />
       </label>
       <input type="submit" value="Submit" />
     </form>
