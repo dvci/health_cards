4664f76c52111ed664948d87b6b233a9e7795d11
mickohanlon23 <mohanlon@mitre.org>
2021-03-03 10:15:30 -0500

    Implement new bundle splitting approach

--- lib/health_cards/bundle_splitting.rb
+++ lib/health_cards/bundle_splitting.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
-require 'json'
-require 'pp'
-require 'hashie'
 
-## May want to change this to constrain health cards
 module BundleSplitting
-  def splitBundle(payload, header_size = 54, signature_size = 95)
-    max_payload_size = 1195 - header_size - signature_size
-    payload
+  MAX_SINGLE_JWS_SIZE = 1195
+  MAX_CHUNK_SIZE = 1191
+
+  def splitBundle(jws) 
+    if (jws.length <= MAX_SINGLE_JWS_SIZE)
+      return [jws]
+    else
+      chunks = []
+      i = 0
+      number_of_chunks = jws.length / MAX_CHUNK_SIZE
+
+      while (i < number_of_chunks)
+        chunks.push(jws.slice(i * MAX_CHUNK_SIZE, (i + 1) * MAX_CHUNK_SIZE))
+        i += 1 
+      end
+
+      return chunks
+      
+    end
   end
 
 end
@@ -15,16 +27,17 @@ end
 
 include BundleSplitting
 
-FILEPATH = 'fixtures/vc-c19-pcr-jwt-payload.json'
-file = File.read(FILEPATH)
-data_hash = JSON.parse(file)
-# pp data_hash
+FILEPATH_SMALL = 'fixtures/example-00-d-jws.txt'
 
-bundle = data_hash['vc']['credentialSubject']['fhirBundle']
+file_data = File.read(FILEPATH_SMALL).split
 
-# pp bundle
-stripped = BundleSplitting.splitBundle(bundle)
+small_jws = file_data[0]
+large_jws = small_jws * 10
 
-pp stripped
+puts small_jws.length
+puts large_jws.length
 
 
+split = BundleSplitting.splitBundle(large_jws)
+
+puts split.length
