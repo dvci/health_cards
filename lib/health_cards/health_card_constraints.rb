52c289da38b400a0202c8921dfe498be1c822173
mickohanlon23 <mohanlon@mitre.org>
2021-03-04 11:47:29 -0500

    Break strip_resource_elements into smaller methods

--- lib/health_cards/health_card_constraints.rb
+++ lib/health_cards/health_card_constraints.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 require 'json'
 require 'pp'
 require 'hashie'
@@ -6,83 +7,89 @@ require 'base64'
 require 'zlib'
 require 'json/minify'
 
-
-## Functionality for "Health Cards are Small section of the Smart Health Cards Specification"
+## Functionality for "Health Cards are Small" section of the Smart Health Cards Specification
 module HealthCardConstraints
-  
   # Remove Extraneous Fields from FHIR Bundle
   def strip_fhir_bundle(bundle)
-
     entries = bundle['entry']
 
-    url_map = Hash.new
-    resource_count = 0;
+    entries, url_map = update_uris(entries)
 
-    # Bundle.entry.fullUrl should be populated with short resource-scheme URIs (e.g., {"fullUrl": "resource:0})
-    entries.each do |entry|
-      old_url = entry["fullUrl"]
+    entries = strip_resource_elements(entries, url_map)
+
+    bundle
+  end
+
+  def update_uris(resources)
+    url_map = {}
+    resource_count = 0
+
+    # Bundle.entry.fullUrl should be populated with short resource-scheme URIs
+    # (e.g., {"fullUrl": "resource:0})
+    resources.each do |entry|
+      old_url = entry['fullUrl']
       new_url = "Resource:#{resource_count}"
 
       url_map[old_url] = new_url
-      entry["fullUrl"] = new_url
+      entry['fullUrl'] = new_url
 
       resource_count += 1
     end
 
-    entries.each do |entry|
+    [resources, url_map]
+  end
 
+  def strip_resource_elements(resources, url_map)
+    resources.each do |entry|
       resource = entry['resource']
 
       # Update references to match new resource-scheme URIs
       HealthCardConstraints.update_links(resource, url_map)
 
       # Remove Resource.id elements
-      resource.delete("id")
+      resource.delete('id')
       # Remove Resource.meta elements
-      resource.delete("meta")
+      resource.delete('meta')
       # Remove Resource.text elements
-      resource.delete("text")
-  
-      resource.each do |element, value|
-        if (value.kind_of?(Hash))
-          if value.key?('coding')
-            # Remove Coding.display elements
-            coding = value['coding']
-            coding.each do |codeableConcept|
-              codeableConcept.delete('display')
-            end
-            # Remove CodeableConcept.text elements
-            value.delete("text")
-          end
-        end
-      end
+      resource.delete('text')
 
+      resource.each do |_element, value|
+        next unless value.is_a?(Hash) && value.key?('coding')
+
+        # Remove Coding.display elements
+        coding = value['coding']
+        coding.each do |codeable_concept|
+          codeable_concept.delete('display')
+        end
+        # Remove CodeableConcept.text elements
+        value.delete('text')
+      end
     end
-  
-    return bundle
+
+    resources
   end
 
   # Payload should be minified (i.e., all optional whitespace is stripped)
   def minify_payload(payload)
     minified_payload = JSON.minify(payload.to_json)
-    return JSON.unparse(minified_payload) 
+    JSON.unparse(minified_payload)
   end
 
-  # Payload should be compressed with the DEFLATE (see RFC1951) algorithm before being signed (note, this should be "raw" DEFLATE compression, omitting any zlib or gz headers)
-    # According to  https://gist.github.com/alazarchuk/8223772181741c4b7a7c
-    # Also references https://agileweboperations.com/2008/09/15/how-inflate-and-deflate-data-ruby-and-php/
-  def gzdeflate (s)
-    Zlib::Deflate.new(nil, -Zlib::MAX_WBITS).deflate(s.to_s, Zlib::FINISH)
+  # Payload should be compressed with the DEFLATE (see RFC1951) algorithm before being signed
+  # (note, this should be "raw" DEFLATE compression, omitting any zlib or gz headers)
+  # According to  https://gist.github.com/alazarchuk/8223772181741c4b7a7c
+  # Also references https://agileweboperations.com/2008/09/15/how-inflate-and-deflate-data-ruby-and-php/
+  def gzdeflate(payload)
+    Zlib::Deflate.new(nil, -Zlib::MAX_WBITS).deflate(payload.to_s, Zlib::FINISH)
   end
 
-  # Reference.reference should be populated with short resource-scheme URIs (e.g., {"patient": {"reference": "resource:0"}})
+  # Reference.reference should be populated with short resource-scheme URIs
+  # (e.g., {"patient": {"reference": "resource:0"}})
   def update_links(hash, mapping)
     hash.each do |k, v|
-      if k == "reference" && v.is_a?(String)
+      if k == 'reference' && v.is_a?(String)
         # update link here
-        if mapping.has_key?(v)
-          v.replace mapping[v]
-        end
+        v.replace mapping[v] if mapping.key?(v)
       elsif v.is_a?(Hash)
         update_links(v, mapping)
       elsif v.is_a?(Array)
@@ -95,15 +102,15 @@ module HealthCardConstraints
   # Main function to operate all of the health card constraints.
   def constrain_health_cards(jws_payload)
     bundle = jws_payload['vc']['credentialSubject']['fhirBundle']
-    if (bundle)
+    if bundle
       strip_fhir_bundle(bundle)
+      pp bundle
       minify_payload(bundle)
       return Base64.encode64(gzdeflate(bundle))
     end
 
-    return bundle
+    bundle
   end
-
 end
 
 include HealthCardConstraints
@@ -112,5 +119,5 @@ FILEPATH = 'fixtures/vc-c19-pcr-jwt-payload.json'
 file = File.read(FILEPATH)
 payload = JSON.parse(file)
 
-puts constrain_health_cards(payload)
-
+constrain_health_cards(payload)
+# puts constrain_health_cards(payload)
