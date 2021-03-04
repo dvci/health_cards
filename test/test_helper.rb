3a9557e57329453db01a6e1cd2e57668f9e54e9b
Stephen MacVicar <smacvicar@mitre.org>
2021-03-04 14:36:58 -0500

    configure simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/test/'
+end
+
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'health_cards'
 
