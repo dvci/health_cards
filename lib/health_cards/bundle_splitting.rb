44783b026815a254a515bb0d5e8648eb47d0e97b
mickohanlon23 <mohanlon@mitre.org>
2021-03-11 07:39:26 -0500

    Add HealthCardConstraints and BundleSplitting modules to HealthCard module

--- lib/health_cards/bundle_splitting.rb
+++ lib/health_cards/bundle_splitting.rb
@@ -1,30 +1,32 @@
 # frozen_string_literal: true
 
-# Split up a JWS into chunks if encoded size is above QR Code Size constraint
-module BundleSplitting
-  MAX_SINGLE_JWS_SIZE = 1195
-  MAX_CHUNK_SIZE = 1191
+module HealthCards
+  # Split up a JWS into chunks if encoded size is above QR Code Size constraint
+  module BundleSplitting
+    MAX_SINGLE_JWS_SIZE = 1195
+    MAX_CHUNK_SIZE = 1191
 
-  def splitBundle(jws)
-    if jws.length <= MAX_SINGLE_JWS_SIZE
-      [jws]
-    else
-      chunks = []
-      i = 0
-      number_of_chunks = jws.length / MAX_CHUNK_SIZE
+    def split_bundle(jws)
+      if jws.length <= MAX_SINGLE_JWS_SIZE
+        [jws]
+      else
+        chunks = []
+        i = 0
+        number_of_chunks = jws.length / MAX_CHUNK_SIZE
 
-      while i < number_of_chunks
-        chunks.push(jws.slice(i * MAX_CHUNK_SIZE, (i + 1) * MAX_CHUNK_SIZE))
-        i += 1
+        while i < number_of_chunks
+          chunks.push(jws.slice(i * MAX_CHUNK_SIZE, (i + 1) * MAX_CHUNK_SIZE))
+          i += 1
+        end
+
+        chunks
+
       end
-
-      chunks
-
     end
   end
 end
 
-include BundleSplitting
+include HealthCards::BundleSplitting
 
 FILEPATH_SMALL = 'fixtures/example-00-d-jws.txt'
 
@@ -33,8 +35,8 @@ file_data = File.read(FILEPATH_SMALL).split
 small_jws = file_data[0]
 large_jws = small_jws * 10
 
-small_split = BundleSplitting.splitBundle(small_jws)
-large_split = BundleSplitting.splitBundle(large_jws)
+small_split = split_bundle(small_jws)
+large_split = split_bundle(large_jws)
 
 puts "Small JWS Payload is #{small_jws.length} characters long and is split into #{small_split.length} chunks."
 puts "Large JWS Payload is #{large_jws.length} characters long and is split into #{large_split.length} chunks."
