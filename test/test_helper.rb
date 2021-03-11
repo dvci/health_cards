f02ad249762e37b87df672ae3ed352d93c71273f
Stephen MacVicar <smacvicar@mitre.org>
2021-03-11 14:12:18 -0500

    configure simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter '/test/'
+  add_filter '/config/'
+end
+
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
