07c6577fd40af6cb6cbaed26780a06735f01d587
Sam Sayer <ssayer@mitre.org>
2021-03-18 13:50:52 -0400

    unify well known configurations

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/.well-known/smart-configuration", to: "well_known#index", as: :well_known, format: :json
+  get "/.well-known/smart-configuration", to: "well_known#smart", as: :well_known_smart, format: :json
   get "/.well-known/jwks", to: "well_known#jwks", as: :well_known_jwks, format: :json
 end
--- test/controllers/well_known_controller_test.rb
+++ test/controllers/well_known_controller_test.rb
@@ -1,22 +1,31 @@
 # frozen_string_literal: true
 
 require 'test_helper'
-require_relative '../../lib/health_cards/issuer'
+require 'health_cards/issuer'
 
 class WellKnownControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @well_known = Rails.application.config.well_known
+    @key_path = @well_known.jwk[:key_path]
+  end
+
+  teardown do
+    FileUtils.rm_rf @key_path
+  end
+
   test 'supports health cards' do
-    get(well_known_url)
+    get(well_known_smart_url)
     assert_response :success
 
     config = JSON.parse(response.body)
-    assert_equal Rails.configuration.well_known, config
+    assert_equal @well_known.smart, config.symbolize_keys
   end
 
   test 'supports jwks' do
     get(well_known_jwks_url)
     assert_response :success
 
-    issuer = HealthCards::Issuer.new Configuration.key_path
+    issuer = HealthCards::Issuer.new @key_path
     assert_equal JSON.pretty_generate(issuer.jwks), response.body
   end
 end
