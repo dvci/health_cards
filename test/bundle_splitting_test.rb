73cc1db9260883140fff5ee2b80bb8e3a3b4a37d
mickohanlon23 <mohanlon@mitre.org>
2021-03-18 06:39:39 -0600

    Rename bundle splitting to chunking

--- test/bundle_splitting_test.rb
+++ test/bundle_splitting_test.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+<<<<<<< HEAD
 
 class BundleSplittingTest < Minitest::Test
   def small_jws_is_not_split
+=======
+require 'health_cards/chunking'
+
+FILEPATH_SMALL = 'test/fixtures/example-00-d-jws.txt'
+FILEPATH_LARGE = 'test/fixtures/example-02-d-jws.txt'
+
+describe HealthCards::Chunking do
+  class DummyClass
+  end
+
+  before(:all) do
+    @dummy_class = DummyClass.new
+    @dummy_class.extend(HealthCards::Chunking)
+>>>>>>> Rename bundle splitting to chunking
   end
 end
