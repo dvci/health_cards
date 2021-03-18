f79af1dfe98cd6570c3f1ce0f0addba1190b9d13
mickohanlon23 <mohanlon@mitre.org>
2021-03-18 06:29:06 -0600

    Fix big in bundle splitting and test updates

--- lib/health_cards/bundle_splitting.rb
+++ lib/health_cards/bundle_splitting.rb
@@ -10,17 +10,11 @@ module HealthCards
       if jws.length <= MAX_SINGLE_JWS_SIZE
         [jws]
       else
-        chunks = []
-        i = 0
-        number_of_chunks = jws.length / MAX_CHUNK_SIZE
+        chunk_count = (jws.length / (MAX_CHUNK_SIZE).to_f).ceil()
+        chunk_size  = (jws.length / (chunk_count).to_f).ceil()
+        chunks = jws.scan(/.{1,#{chunk_size}}/)
 
-        while i < number_of_chunks
-          chunks.push(jws.slice(i * MAX_CHUNK_SIZE, (i + 1) * MAX_CHUNK_SIZE))
-          i += 1
-        end
-
-        chunks
-
+        return chunks
       end
     end
   end
@@ -28,17 +22,27 @@ end
 
 # include HealthCards::BundleSplitting
 
+
 # FILEPATH_SMALL = 'fixtures/example-00-d-jws.txt'
+# FILEPATH_LARGE = 'fixtures/example-02-d-jws.txt'
 
-# file_data = File.read(FILEPATH_SMALL).split
+# file_data_small = File.read(FILEPATH_SMALL).split
+# file_data_large = File.read(FILEPATH_LARGE).split
 
-# small_jws = file_data[0]
-# large_jws = small_jws * 10
+# small_jws = file_data_small[0]
+# #large_jws = small_jws * 10
+# large_jws = file_data_large[0]
 
 # small_split = split_bundle(small_jws)
 # large_split = split_bundle(large_jws)
 
 # puts "Small JWS Payload is #{small_jws.length} characters long and is split into #{small_split.length} chunks."
 # puts "Large JWS Payload is #{large_jws.length} characters long and is split into #{large_split.length} chunks."
-# puts
-# puts large_split
+
+# large_split.each do |piece|
+#   puts
+#   puts piece.length
+#   puts 
+#   puts piece
+ 
+# end
