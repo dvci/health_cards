77fdaf7ab3a38c388210a959382a0b3a3f758b9e
pmadria <pmadria@mitre.org>
2021-03-22 08:41:47 -0500

    just trying different things to add the button

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -23,6 +23,11 @@ class PatientsController < ApplicationController
     @patient = Patient.new(params)
   end
 
+    # GET /patients/demo_patient
+    def new
+      @patient = Patient.new
+    end
+
   # GET /patients/1/edit
   def edit; end
 
