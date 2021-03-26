4bf29f301d6046524218811dee36de6fc8bf80ec
Dan Lee <danlee1025@gmail.com>
2021-03-26 15:10:12 -0400

    Split jws into numeric chunks to be encoded into QR code.

--- lib/health_cards/chunking.rb
+++ lib/health_cards/chunking.rb
@@ -13,8 +13,20 @@ module HealthCards
         chunk_count = (jws.length / MAX_CHUNK_SIZE.to_f).ceil
         chunk_size  = (jws.length / chunk_count.to_f).ceil
         jws.scan(/.{1,#{chunk_size}}/)
-
       end
+    end
+
+    # Splits jws into chunks and converts each string into numeric
+    def generate_qr_chunks(jws)
+      jws_chunks = split_bundle jws
+      jws_chunks.map { |c| convert_jws_to_numeric(c) }
+    end
+
+    private
+
+    # Each character "c" of the jws is converted into a sequence of two digits by taking c.ord - 45
+    def convert_jws_to_numeric(jws)
+      jws.chars.map { |c| c.ord - 45 }.join
     end
   end
 end
