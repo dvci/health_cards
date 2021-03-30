f07d4792268d77b3e567cf438b486b095f5e9d06
Mick O'Hanlon <mohanlon@mitre.org>
2021-03-30 10:14:24 -0400

    Small Health Cards (#20)
    * FHIR Bundle Constraints
    
    * Finish bundle constraints
    
    * initial frontend commit
    
    * replace yarn with npm
    
    * add basic patient form
    
    * Revert "add basic patient form"
    
    This reverts commit 8756f09a01b8dd7cdd843c522be9ae5e193a5857.
    
    * Revert "replace yarn with npm"
    
    This reverts commit c31b1fb8eaa3221c035d6d09069ed1e916198e5c.
    
    * Revert "initial frontend commit"
    
    This reverts commit aed818b24b257d3d1a874aa3a0c1ee9b6a63ff7b.
    
    * Link together functions of HealthCardConstraints module
    
    * Set up tests for bundle splitting
    
    * Implement new bundle splitting approach
    
    * Clean up testing for bundle splitting
    
    * Break strip_resource_elements into smaller methods
    
    * Rubocop style fixes
    
    * Delete extraneous comments
    
    * Remove hashie from Gemfile. Remove unnecessary scope declaration
    
    * Add HealthCardConstraints and BundleSplitting modules to HealthCard module
    
    * Set up fixtures for bundle splitting test
    
    * Bundle Splitting test updates
    
    * Fix big in bundle splitting and test updates
    
    * Comment out broken code
    
    * Remove bundle_splitting files
    
    * Refactor tests to follow minitest
    
    * Recursively find CodeableConcept and Coding, and finish writing tests
    
    * Remove extra fixtures files
    
    * Change bundle_operations method name to strip_fhir_bundle
    
    * Change 'resources' variable name to 'entries', and remove redundant return after .each block
    
    * Rename tests with method name as subject
    
    * Move HealthCardConstraints module methods into VerifiableCredential class
    
    * Adjust tests to use verifiable credential class
    
    * FHIR Bundle Constraints
    
    * Finish bundle constraints
    
    * initial frontend commit
    
    * replace yarn with npm
    
    * add basic patient form
    
    * Revert "add basic patient form"
    
    This reverts commit 8756f09a01b8dd7cdd843c522be9ae5e193a5857.
    
    * Revert "replace yarn with npm"
    
    This reverts commit c31b1fb8eaa3221c035d6d09069ed1e916198e5c.
    
    * Revert "initial frontend commit"
    
    This reverts commit aed818b24b257d3d1a874aa3a0c1ee9b6a63ff7b.
    
    * Link together functions of HealthCardConstraints module
    
    * Set up tests for bundle splitting
    
    * Implement new bundle splitting approach
    
    * Clean up testing for bundle splitting
    
    * Break strip_resource_elements into smaller methods
    
    * Rubocop style fixes
    
    * Delete extraneous comments
    
    * Remove hashie from Gemfile. Remove unnecessary scope declaration
    
    * Add HealthCardConstraints and BundleSplitting modules to HealthCard module
    
    * Set up fixtures for bundle splitting test
    
    * Bundle Splitting test updates
    
    * Fix big in bundle splitting and test updates
    
    * Comment out broken code
    
    * Remove bundle_splitting files
    
    * Refactor tests to follow minitest
    
    * Recursively find CodeableConcept and Coding, and finish writing tests
    
    * Remove extra fixtures files
    
    * Change bundle_operations method name to strip_fhir_bundle
    
    * Change 'resources' variable name to 'entries', and remove redundant return after .each block
    
    * Rename tests with method name as subject
    
    * Move HealthCardConstraints module methods into VerifiableCredential class
    
    * Adjust tests to use verifiable credential class
    
    * Remove old files relating to HealthCardConstraints module
    
    * clean up verifiable credential test
    
    * Remove unused testing file
    
    * Remove old HealthCardConstraint module and associated files
    
    Co-authored-by: Reece Adamson <radamson@mitre.org>

--- lib/health_cards/verifiable_credential.rb
+++ lib/health_cards/verifiable_credential.rb
@@ -44,13 +44,81 @@ module HealthCards
 
     def jwt; end
 
+    def strip_fhir_bundle
+      stripped_bundle = @fhir_bundle.dup
+      if stripped_bundle.key?('entry') && stripped_bundle['entry'].length.positive?
+        entries = stripped_bundle['entry']
+        entries, @url_map = redefine_uris(entries)
+        update_elements(entries)
+      end
+      stripped_bundle
+    end
+
+    def minify_payload
+      JSON.minify(credential.to_json)
+    end
+
+    # According to  https://gist.github.com/alazarchuk/8223772181741c4b7a7c
+    # Also references https://agileweboperations.com/2008/09/15/how-inflate-and-deflate-data-ruby-and-php/
+    def compress_credential
+      deflated = Zlib::Deflate.new(nil, -Zlib::MAX_WBITS).deflate(minify_payload.to_s, Zlib::FINISH)
+      Base64.encode64(deflated)
+    end
+
     private
 
     def credential_subject
       {
         fhirVersion: FHIR_VERSION,
-        fhirBundle: fhir_bundle
+        fhirBundle: strip_fhir_bundle
       }.tap { |subject| subject[:id] = subject_id if subject_id }
+    end
+
+    # Helper methods for strip_fhir_bundle
+
+    def redefine_uris(entries)
+      url_map = {}
+      resource_count = 0
+      entries.each do |entry|
+        old_url = entry['fullUrl']
+        new_url = "resource:#{resource_count}"
+        url_map[old_url] = new_url
+        entry['fullUrl'] = new_url
+        resource_count += 1
+      end
+      [entries, url_map]
+    end
+
+    def update_elements(entries)
+      entries.each do |entry|
+        resource = entry['resource']
+        resource.delete('id')
+        resource.delete('meta')
+        resource.delete('text')
+        update_nested_elements(resource)
+      end
+    end
+
+    def update_nested_elements(hash) # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
+      hash.each do |k, v|
+        if v.is_a?(Hash) && (k.include?('CodeableConcept') || v.key?('coding'))
+          v.delete('text')
+        elsif k == 'coding'
+          v.each do |coding|
+            coding.delete('display')
+          end
+        elsif k == 'reference' && v.is_a?(String)
+          v.replace @url_map[v] if @url_map.key?(v)
+        end
+
+        case v
+        when Hash
+          update_nested_elements(v)
+        when Array
+          v.flatten.each { |x| update_nested_elements(x) if x.is_a?(Hash) }
+        end
+      end
+      hash
     end
   end
 end
