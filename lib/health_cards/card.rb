1a75671cc4321828441170569ce2aeba849982ad
Sam Sayer <ssayer@mitre.org>
2021-04-02 13:00:54 -0400

    refactor cards and keys

---
+++ lib/health_cards/card.rb
@@ -0,0 +1,52 @@
+module HealthCards
+
+  # attr_acessor 
+
+  # Handle JWS with non-JSON Payloads
+  class Card
+
+    def initialize(key, payload)
+      @key = key
+      @payload = payload
+    end
+
+    def jws_signature
+      signature = @key.sign(jws_payload)
+      encode(signature)
+    end
+
+    def jose_header
+      encode(
+        JSON.generate({
+                        zip: "DEF",
+                        alg: "ES256",
+                        kid: @key.thumbprint
+                      }
+        )
+      )
+    end
+
+    def jws_payload
+      encode(@payload)
+    end
+
+    def encode(data)
+      Base64.urlsafe_encode64(data, padding: false).gsub("\n", "")
+    end
+
+    def decode(data)
+      Base64.urlsafe_decode64(data)
+    end
+
+    def jws
+      [jose_header, jws_payload, jws_signature].join('.')
+    end
+
+    def self.verify(jws)
+      _, payload_to_verify, signature_to_verify = jws.split('.')
+      verified = @key.verify(payload_to_verify, signature_to_verify)
+      [verified, decode(payload_to_verify)]
+    end
+
+
+  end
---
+++ lib/health_cards/key.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'openssl'
+require 'json/jwt'
+
+module HealthCards
+  # Methods to generate signing keys and jwk
+  class Key
+    class << self
+      def load_file(path)
+        key = nil
+        if File.exist?(path)
+          key = OpenSSL::PKey::EC.new(File.read(path))
+        else
+          key = OpenSSL::PKey::EC.generate('prime256v1')
+          File.write(path, key.to_pem)
+        end
+        byebug
+        Key.new(key)
+      end
+
+      def load_url
+        # TODO
+      end
+    end
+
+    def initialize(key)
+      @key = key
+    end
+
+    def thumbprint
+      @key.to_jwk.thumbprint
+    end
+
+    def sign(payload)
+      signing_key.dsa_sign_asn1(jws_payload)
+    end
+
+    def verify(payload, signature)
+      public_key.dsa_verify_asn1(payload, signature)
+    end
+
+    def signing_key
+      @signing_key ||= @key
+    end
+
+    def public_key
+      @public_key ||= signing_key.public_key
+    end
+
+    def to_json(*_args)
+      @jwks ||= JSON::JWK::Set.new(keys: [public_key]).as_json
+    end
+    
+  end
+end
