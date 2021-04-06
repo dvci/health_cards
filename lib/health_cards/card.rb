35557a52cc2916b40e0293e8d43f0ec9749a16b5
Reece Adamson <radamson@mitre.org>
2021-04-06 10:12:44 -0400

    add docs to `card`

--- lib/health_cards/card.rb
+++ lib/health_cards/card.rb
@@ -10,25 +10,41 @@ module HealthCards
     attr_accessor :payload
 
     class << self
+      # Encodes the provided data using url safe base64 without padding
+      # @param data [String] the data to be encoded
+      # @return [String] the encoded data
       def encode(data)
         Base64.urlsafe_encode64(data, padding: false).gsub("\n", '')
       end
 
+      # Decodes the provided data using url safe base 64
+      # @param data [String] the data to be decoded
+      # @return [String] the decoded data
       def decode(data)
         Base64.urlsafe_decode64(data)
       end
 
+      # Creates a Card from a JWS
+      # @param jws [String] the JWS string
+      # @param public_key [HealthCards::PublicKey] the public key associated with the JWS
+      # @param private_key [HealthCards::PublicKey] the private key associated with the JWS
+      # @return [HealthCards::Card]
       def from_jws(jws, public_key: nil, private_key: nil)
         header, payload, signature = jws.split('.').map { |entry| decode(entry) }
         Card.new(header: header, payload: payload, signature: signature,
                  public_key: public_key, private_key: private_key)
       end
 
+      # Verify the JWS using the public key
+      # @param jws [String] the JWS
+      # @param key [HealthCards::PublicKey] the public key
+      # @return [Boolean]
       def verify(jws, key)
         Card.from_jws(jws, public_key: key).verify
       end
     end
 
+    # Create a Card
     def initialize(payload:, private_key: nil, public_key: nil, header: nil, signature: nil)
       @payload = payload
       @private_key = private_key
@@ -39,12 +55,10 @@ module HealthCards
       @header = header
     end
 
+    # Export the card to a JWS
+    # @return [String] the JWS
     def to_jws
       [header, payload, signature].map { |entry| Card.encode(entry) }.join('.')
-    end
-
-    def encoded_payload
-      Card.encode(payload)
     end
 
     def private_key=(new_private_key)
@@ -57,6 +71,8 @@ module HealthCards
       @public_key = new_public_key
     end
 
+    # The header component of the card
+    # @return [String] the header
     def header
       return @header if @header
 
@@ -69,6 +85,8 @@ module HealthCards
                                 })
     end
 
+    # The signature component of the card
+    # @return [String] the unencoded signature
     def signature
       return @signature if @signature
 
@@ -77,6 +95,8 @@ module HealthCards
       @signature ||= private_key.sign(encoded_payload)
     end
 
+    # Verify the digital signature on the card
+    # @return [Boolean]
     def verify
       raise MissingPublicKey unless public_key
 
@@ -91,6 +111,10 @@ module HealthCards
 
     def reset_header
       @header = nil
+    end
+
+    def encoded_payload
+      Card.encode(payload)
     end
 
     # Thrown when attempting to sign a card without providing a private key
