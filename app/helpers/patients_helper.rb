b58ae930a562df8bc571248d3cfd715590938823
pmadria <65190574+pmadria@users.noreply.github.com>
2021-04-06 14:13:03 -0500

    Demo Data for new patient (#19)
    * just trying different things to add the button
    
    * some changes
    
    * button works
    
    * rebase
    
    * rebasing
    
    * some changes
    
    * updated after rebase
    
    * rebase
    
    * updated patients_helper
    
    * merge conflicts fixed
    
    * just trying different things to add the button
    
    * button works
    
    * rebase
    
    * updated patients_helper
    
    * changes on rebase
    
    * resolved all merge conflicts
    
    * latest changes
    
    * fix patient parameters for demo values
    
    * rebase
    
    * rebasing
    
    * some changes
    
    * updated after rebase
    
    * just trying different things to add the button
    
    * button works
    
    * rebase
    
    * updated patients_helper
    
    * just trying different things to add the button
    
    * button works
    
    * rebase
    
    * updated patients_helper
    
    * latest changes
    
    * fix patient parameters for demo values
    
    * all the fields are populating except birthday and the button is in the top at the moment
    
    * pushing changes
    
    * rubocop
    
    * date shows up after a small fix
    
    * fixed the double image issue
    
    * made all the stylistic changes
    
    * date works with both safari and chrome
    
    Co-authored-by: Sam Sayer <ssayer@mitre.org>

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -4,4 +4,27 @@ module PatientsHelper
   def show_address(patient)
     [patient.street_line1, patient.street_line2, patient.city, patient.state, patient.zip_code].compact.join(', ')
   end
+
+  def fake_patient_params
+    gender = Patient::GENDERS.sample
+    phone = Faker::PhoneNumber.phone_number
+    email = Faker::Internet.email
+    street_line1 = Faker::Address.street_address
+    city = Faker::Address.city
+    state = Faker::Address.state_abbr
+    zip_code = Faker::Address.zip_code
+    birth_date = Faker::Date.birthday(min_age: 16, max_age: 100)
+    street_line2 = Faker::Address.secondary_address
+    given = case gender
+            when 'male'
+              Faker::Name.male_first_name
+            when 'female'
+              Faker::Name.female_first_name
+            else
+              Faker::Name.first_name
+            end
+    { patient: { given: given, family: Faker::Name.last_name, gender: gender,
+                 birth_date: birth_date, phone: phone, email: email, street_line1: street_line1,
+                 street_line2: street_line2, city: city, state: state, zip_code: zip_code } }
+  end
 end
