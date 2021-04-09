d126f5c63a77b6d39724cd74de82de9a0945eb3b
Reece Adamson <radamson@mitre.org>
2021-04-09 10:32:30 -0400

    verifier

---
+++ lib/health_cards/verifier.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+module HealthCards
+  # Verifiers can validate HealthCards using public keys
+  class Verifier
+
+    attr_reader :keys
+
+    # Create a new Verifier
+    #
+    # @param keys [HealthCards::KeySet, HealthCards::Key, nil]
+    def initialize(keys: nil)
+      @keys = case keys
+              when KeySet
+                   keys
+              when Key
+                   KeySet.new(keys)
+              else
+                   raise ArgumentError
+              end
+    end
+
+    # Add a key to use when verifying
+    #
+    # @param key [HealthCards::Key] the key to add
+    def add_key(key)
+      @keys.add_key(key)
+    end
+
+    # Remove a key to use when verifying
+    #
+    # @param key [HealthCards::Key] the key to remove
+    def remove_key(key)
+      @keys.remove_key(key)
+    end
+
+    # Verify a HealthCard
+    #
+    # @param health_card [HealthCards::HealthCard] the health card to verify
+    def verify(health_card)
+      # TODO: This needs better logic to make sure the public key is correct and check for key resolution
+      health_card.verify
+    end
+  end
+end
