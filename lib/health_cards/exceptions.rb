41f9ef12732231b9b6a9a70417919431de06a834
Reece Adamson <radamson@mitre.org>
2021-04-09 10:38:42 -0400

    add exceptions to exceptions.rb

--- lib/health_cards/exceptions.rb
+++ lib/health_cards/exceptions.rb
@@ -7,4 +7,11 @@ module HealthCards
       super(msg)
     end
   end
+
+  # Exception thrown when a public key is expected or required
+  class MissingPublicKey < StandardError
+    def initialize(msg = 'Missing public key')
+      super(msg)
+    end
+  end
 end
