8f8189714c192141e0d87665b56653e3a7442de0
Dan Lee <danlee1025@gmail.com>
2021-04-15 11:32:45 -0400

    Updated lookup for vaccine.

--- app/helpers/health_cards_helper.rb
+++ app/helpers/health_cards_helper.rb
@@ -15,16 +15,10 @@ module HealthCardsHelper
 
   def create_immunizations(pat, bundle)
     immunizations = bundle.entry.select { |e| e.resource.is_a?(FHIR::Immunization) }
-    vaccine_id_map = {
-      '207': 1,
-      '208': 2,
-      '212': 3
-    }
-
     immunizations.each do |i|
       immunization_resource = i.resource
       pat.immunizations.new({
-                              vaccine_id: vaccine_id_map[immunization_resource.vaccineCode.coding[0].code.to_sym],
+                              vaccine_id: Vaccine.find_by(code: immunization_resource.vaccineCode.coding[0].code).id,
                               lot_number: immunization_resource.lotNumber,
                               occurrence: immunization_resource.occurrenceDateTime
                             })
