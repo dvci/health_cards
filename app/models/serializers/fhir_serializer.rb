49cebb9b8d17ca9e8cdb965c3832029b31824bda
Stephen MacVicar <smacvicar@mitre.org>
2021-04-20 12:48:38 -0400

    add Dockerfile

--- app/models/serializers/fhir_serializer.rb
+++ app/models/serializers/fhir_serializer.rb
@@ -1,17 +1,19 @@
 # frozen_string_literal: true
 
 # Manages serializing from fhir_models object into FHIR JSON to be stored in the database
-module FHIRSerializer
-  def load(json)
-    json ? FHIR.from_contents(json) : new
-  end
+module Serializers
+  module FHIRSerializer
+    def load(json)
+      json ? FHIR.from_contents(json) : new
+    end
 
-  def dump(model)
-    raise ActiveRecord::SerializationTypeMismatch unless model.class.module_parent == FHIR
+    def dump(model)
+      raise ActiveRecord::SerializationTypeMismatch unless model.class.module_parent == FHIR
 
-    model.to_json
+      model.to_json
+    end
   end
 end
 
-FHIR::Patient.class_eval { extend FHIRSerializer }
-FHIR::Immunization.class_eval { extend FHIRSerializer }
+FHIR::Patient.class_eval { extend Serializers::FHIRSerializer }
+FHIR::Immunization.class_eval { extend Serializers::FHIRSerializer }
