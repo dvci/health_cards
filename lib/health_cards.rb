037d90c447c6c526c47da974db04b382e9294919
Sam Sayer <ssayer@mitre.org>
2021-04-26 22:30:01 -0400

    remove verifiable credential

--- lib/health_cards.rb
+++ lib/health_cards.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require 'health_cards/version'
-require 'health_cards/verifiable_credential'
 require 'health_cards/issuer'
 require 'health_cards/key'
 require 'health_cards/jws'
