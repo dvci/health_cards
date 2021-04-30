e4fc262d7650a183eb11df0384410f6d4b8ec54d
Reece Adamson <radamson@mitre.org>
2021-04-30 11:35:48 -0400

    use fhir_models

--- db/migrate/20210428145300_add_id_to_fhir_json.rb
+++ db/migrate/20210428145300_add_id_to_fhir_json.rb
@@ -1,28 +1,38 @@
 # frozen_string_literal: true
 
+
 # Sets ID in existing FHIR-based records
 class AddIdToFHIRJson < ActiveRecord::Migration[6.1]
   # https://guides.rubyonrails.org/v4.1/migrations.html#using-models-in-your-migrations
   # https://rails.rubystyle.guide/#define-model-class-migrations
+  #
+  class MigrationFHIRSerializer
+    def self.load(json)
+      json ? FHIR.from_contents(json) : new
+    end
 
-  class MigrationPatient < ActiveRecord::Base
-    self.table_name = :patients
-    serialize :json, JSON
+    def self.dump(model)
+      raise ActiveRecord::SerializationTypeMismatch unless model.class.module_parent == FHIR
+
+      model.to_json
+    end
+  end
+
+  class MigrateFHIRRecord
+    serialize :json, MigrationFHIRSerializer
 
     def set_fhir_id
-      json['id'] = id
+      json.id = id
       save!
     end
   end
 
-  class MigrationImmunization < ActiveRecord::Base
-    self.table_name = :immunizations
-    serialize :json, JSON
+  class MigrationPatient < MigrateFHIRRecord
+    self.table_name = :patients
+  end
 
-    def set_fhir_id
-      json['id'] = id
-      save!
-    end
+  class MigrationImmunization < MigrateFHIRRecord
+    self.table_name = :immunizations
   end
 
   def change
