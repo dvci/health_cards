4483170caf7df77cd34eb0d504c60ebfba71a700
Reece Adamson <radamson@mitre.org>
2021-04-30 11:01:06 -0400

    update migration

--- db/migrate/20210428145300_add_id_to_fhir_json.rb
+++ db/migrate/20210428145300_add_id_to_fhir_json.rb
@@ -2,8 +2,27 @@
 
 # Sets ID in existing FHIR-based records
 class AddIdToFHIRJson < ActiveRecord::Migration[6.1]
+  # https://guides.rubyonrails.org/v4.1/migrations.html#using-models-in-your-migrations
+  # https://rails.rubystyle.guide/#define-model-class-migrations
+
+  class MigrationPatient < ActiveRecord::Base
+    self.table_name = :patients
+    def set_fhir_id
+      json.id = id
+      save!
+    end
+  end
+
+  class MigrationImmunization < ActiveRecord::Base
+    self.table_name = :immunizations
+    def set_fhir_id
+      json.id = id
+      save!
+    end
+  end
+
   def change
-    Patient.all.each { |pat| pat.set_fhir_id }
-    Immunization.all.each { |imm| imm.set_fhir_id }
+    MigrationPatient.all.each { |pat| pat.set_fhir_id }
+    MigrationImmunization.all.each { |imm| imm.set_fhir_id }
   end
 end
