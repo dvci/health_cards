a872d53f4f4fbc08208ef532c65ba9d60cdd11bd
Reece Adamson <41651655+radamson@users.noreply.github.com>
2021-05-03 08:27:56 -0400

    Update Migration (#32)
    * update migration
    
    * update migration
    
    * use fhir_models
    
    * fix MigrateFHIRRecord to properly inherit from ActiveRecord::Base

--- db/migrate/20210428145300_add_id_to_fhir_json.rb
+++ db/migrate/20210428145300_add_id_to_fhir_json.rb
@@ -1,9 +1,42 @@
 # frozen_string_literal: true
 
+
 # Sets ID in existing FHIR-based records
 class AddIdToFHIRJson < ActiveRecord::Migration[6.1]
+  # https://guides.rubyonrails.org/v4.1/migrations.html#using-models-in-your-migrations
+  # https://rails.rubystyle.guide/#define-model-class-migrations
+  #
+  class MigrationFHIRSerializer
+    def self.load(json)
+      json ? FHIR.from_contents(json) : new
+    end
+
+    def self.dump(model)
+      raise ActiveRecord::SerializationTypeMismatch unless model.class.module_parent == FHIR
+
+      model.to_json
+    end
+  end
+
+  class MigrateFHIRRecord < ActiveRecord::Base
+    serialize :json, MigrationFHIRSerializer
+
+    def set_fhir_id
+      json.id = id
+      save!
+    end
+  end
+
+  class MigrationPatient < MigrateFHIRRecord
+    self.table_name = :patients
+  end
+
+  class MigrationImmunization < MigrateFHIRRecord
+    self.table_name = :immunizations
+  end
+
   def change
-    Patient.all.each { |pat| pat.set_fhir_id }
-    Immunization.all.each { |imm| imm.set_fhir_id }
+    MigrationPatient.all.each { |pat| pat.set_fhir_id }
+    MigrationImmunization.all.each { |imm| imm.set_fhir_id }
   end
 end
