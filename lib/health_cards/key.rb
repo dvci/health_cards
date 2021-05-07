f1d515d19a314127d7ac7ccadc7ecfe7a67c17fc
Reece Adamson <radamson@mitre.org>
2021-05-07 17:25:22 -0400

    use the binary bn public key instead of hex

--- lib/health_cards/key.rb
+++ lib/health_cards/key.rb
@@ -6,7 +6,6 @@ require 'base64'
 module HealthCards
   # Methods to generate signing keys and jwk
   class Key
-    BASE = { kty: 'EC', crv: 'P-256' }.freeze
     DIGEST = OpenSSL::Digest.new('SHA256')
 
     # Checks if obj is the the correct key type or nil
@@ -47,15 +46,17 @@ module HealthCards
     end
 
     def coordinates
-      unless @coordinates
-        coor = @key.private_key? ? { d: @key.private_key.to_s(16) } : {}
-        key_hex = @key.public_key.to_bn.to_s(16)
-        xy = { x: key_hex[2..65], y: key_hex[66..130] }
-        @coordinates = coor.merge(xy).transform_values do |val|
-          Base64.urlsafe_encode64([val].pack('H*'), padding: false)
-        end
+      return @coordinates if @coordinates
+
+      key_binary = @key.public_key.to_bn.to_s(2)
+      coords = { kty: 'EC',
+             crv: 'P-256',
+             x: key_binary[1,key_binary.length/2],
+             y: key_binary[key_binary.length/2+1,key_binary.length] }
+      coords[:d] = @key.private_key.to_s(2) if @key.private_key?
+      @coordinates = coords.transform_values do |val|
+        Base64.urlsafe_encode64(val, padding: false)
       end
-      BASE.merge(@coordinates)
     end
   end
 end
