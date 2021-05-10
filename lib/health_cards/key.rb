d793e4f4fdc660eb7c3c7c11c90364ac75f40aff
Reece Adamson <radamson@mitre.org>
2021-05-10 10:25:42 -0400

    fix JWK kty and crv being erroneously b64 encoded

--- lib/health_cards/key.rb
+++ lib/health_cards/key.rb
@@ -6,6 +6,7 @@ require 'base64'
 module HealthCards
   # Methods to generate signing keys and jwk
   class Key
+    BASE = { kty: 'EC', crv: 'P-256' }.freeze
     DIGEST = OpenSSL::Digest.new('SHA256')
 
     # Checks if obj is the the correct key type or nil
@@ -53,14 +54,12 @@ module HealthCards
       return @coordinates if @coordinates
 
       key_binary = @key.public_key.to_bn.to_s(2)
-      coords = { kty: 'EC',
-                 crv: 'P-256',
-                 x: key_binary[1, key_binary.length / 2],
+      coords = { x: key_binary[1, key_binary.length / 2],
                  y: key_binary[key_binary.length / 2 + 1, key_binary.length] }
       coords[:d] = @key.private_key.to_s(2) if @key.private_key?
       @coordinates = coords.transform_values do |val|
         Base64.urlsafe_encode64(val, padding: false)
-      end
+      end.merge(BASE)
     end
   end
 end
