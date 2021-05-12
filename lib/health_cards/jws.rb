f1d5aedd3727facd30b1e4e5359fe6781f8964e6
Reece Adamson <radamson@mitre.org>
2021-05-12 10:47:50 -0400

    have jws use encoding module

--- lib/health_cards/jws.rb
+++ lib/health_cards/jws.rb
@@ -4,19 +4,7 @@ module HealthCards
   # Create JWS from a payload
   class JWS
     class << self
-      # Encodes the provided data using url safe base64 without padding
-      # @param data [String] the data to be encoded
-      # @return [String] the encoded data
-      def encode(data)
-        Base64.urlsafe_encode64(data, padding: false).gsub("\n", '')
-      end
-
-      # Decodes the provided data using url safe base 64
-      # @param data [String] the data to be decoded
-      # @return [String] the decoded data
-      def decode(data)
-        Base64.urlsafe_decode64(data)
-      end
+      include Encoding
 
       # Creates a Card from a JWS
       # @param jws [String, HealthCards::JWS] the JWS string
