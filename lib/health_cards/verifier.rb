7c478f65f9a34879a7ed1df46d196858a6058fbd
Reece Adamson <radamson@mitre.org>
2021-05-12 14:56:46 -0400

    add verify_using_key_set class method

--- lib/health_cards/verifier.rb
+++ lib/health_cards/verifier.rb
@@ -15,9 +15,23 @@ module HealthCards
     # @param verifiable [HealthCards::JWS, String] the health card to verify
     # @return [Boolean]
     def self.verify(verifiable)
+      verify_using_key_set(verifiable)
+    end
+
+    # Verify Health Card with given KeySet
+    #
+    # @param verifiable [HealthCards::JWS, String] the health card to verify
+    # @param key_set [HealthCards::KeySet, nil] the KeySet from which keys should be taken or added
+    # @param resolve_keys [Boolean] if keys should be resolved
+    # @return [Boolean]
+    def self.verify_using_key_set(verifiable, key_set = nil, resolve_keys = true)
       jws = JWS.from_jws(verifiable)
-      key_set = resolve_key(jws)
+      key_set ||= HealthCards::KeySet.new
+      key_set.add_keys(resolve_key(jws)) if resolve_keys && key_set.find_key(jws.kid).nil?
+
       key = key_set.find_key(jws.kid)
+      raise MissingPublicKey, 'Verifier does not contain public key that is able to verify this signature' unless key
+
       jws.public_key = key
       jws.verify
     end
@@ -66,16 +80,7 @@ module HealthCards
     # @param verifiable [HealthCards::JWS, String] the health card to verify
     # @return [Boolean]
     def verify(verifiable)
-      # TODO: This needs better logic to make sure the public key is correct and check for key resolution
-      jws = JWS.from_jws(verifiable)
-
-      add_keys(self.class.resolve_key(jws)) if resolve_keys? && @keys.find_key(jws.kid).nil?
-
-      key = keys.find_key(jws.kid)
-      raise MissingPublicKey, 'Verifier does not contain public key that is able to verify this signature' unless key
-
-      jws.public_key = key
-      jws.verify
+      self.class.verify_using_key_set(verifiable, keys, resolve_keys?)
     end
 
     def resolve_keys?
