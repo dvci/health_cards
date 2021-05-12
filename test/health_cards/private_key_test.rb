03e9cee11f5c17596b54dc926be6070e9dab66cd
Reece Adamson <radamson@mitre.org>
2021-05-12 10:24:10 -0400

    non-deterministic signing

--- test/health_cards/private_key_test.rb
+++ test/health_cards/private_key_test.rb
@@ -5,11 +5,11 @@ require 'test_helper'
 class PrivateKeyTest < Minitest::Test
   def setup
     # Key from https://datatracker.ietf.org/doc/html/rfc7515#appendix-A.3.1
-    @jwk = {"kty":"EC",
-            "crv":"P-256",
-            "x":"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
-            "y":"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
-            "d":"jpsQnnGQmL-YBIffH1136cspYG6-0iY7X1fCE9-E9LI"
+    @jwk = {kty: "EC",
+            crv: "P-256",
+            x:"f83OJ3D2xF1Bg8vub9tLe1gHMzV76e8Tus9uPHvRVEU",
+            y:"x_FEzRu9m36HLN_tue659LNpXW6pCyStikYjKIWI5a0",
+            d:"jpsQnnGQmL-YBIffH1136cspYG6-0iY7X1fCE9-E9LI"
     }
 
     @key = HealthCards::PrivateKey.from_jwk(@jwk)
@@ -23,5 +23,22 @@ class PrivateKeyTest < Minitest::Test
     payload = 'eyJhbGciOiJFUzI1NiJ9.eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQogImh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ'
     signature = @key.sign(payload)
     assert_equal @encoder.encode(signature), 'DtEhU3ljbEg8L38VWAfUAqOyKAM6-Xx-F4GawxaepmXFCgfTjDxw5djxLa8ISlSApmWQxfKTUJqPP3-Kg6NU1Q'
+  end
+
+  def test_keys_from_jwk_sign_deterministically
+    key1 = HealthCards::PrivateKey.from_jwk(@jwk)
+    key2 = HealthCards::PrivateKey.from_jwk(@jwk)
+    assert_equal key1.kid, key2.kid
+
+    assert_equal @jwk[:x], key1.coordinates[:x]
+    assert_equal @jwk[:y], key1.coordinates[:y]
+
+    puts key1.coordinates[:d]
+    assert_equal @jwk[:d], key1.coordinates[:d]
+    assert_equal key1.coordinates[:d], key2.coordinates[:d]
+
+    payload = 'foo'
+    assert_equal key1.sign(payload), key1.sign(payload)
+    assert_equal key1.sign(payload), key2.sign(payload)
   end
 end
