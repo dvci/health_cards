3155e4d0cec0eda1210171422cb1a69b1f3d5c2d
pmadria <pmadria@mitre.org>
2021-05-18 10:58:01 -0500

    added #immunization

--- lib/health_cards/covid_health_card.rb
+++ lib/health_cards/covid_health_card.rb
@@ -5,6 +5,7 @@ module HealthCards
   class COVIDHealthCard < HealthCards::HealthCard
     fhir_version '4.0.1'
     additional_types 'https://smarthealth.cards#covid19'
+    additional_types 'https://smarthealth.cards#immunization'
 
     allow FHIR::Patient, %w[name birthDate]
     allow FHIR::Immunization, %w[status vaccineCode patient occurrenceDateTime]
