14b60f9d6643db0546c83c37325de575668e44e1
pmadria <pmadria@mitre.org>
2021-05-26 09:32:50 -0500

    Changes based on Mick's comments

--- app/javascript/packs/details_pg.js
+++ app/javascript/packs/details_pg.js
@@ -1,14 +1,14 @@
 if (document.readyState !== "loading") {
     onReady();
-} else {
+}   else {
     document.addEventListener("DOMContentLoaded", onReady);
 }
 
 function onReady() {
     let cardToggles = document.getElementsByClassName('card-toggle');
-	for (let i = 0; i < cardToggles.length; i++) {
-		cardToggles[i].addEventListener('click', e => {
+    for (let i = 0; i < cardToggles.length; i++) {
+        cardToggles[i].addEventListener('click', e => {
             e.currentTarget.parentElement.parentElement.childNodes[3].classList.toggle('is-hidden');
-		});
+        });
     }
 }
