48a9512b23098798b140c4a1e069ecb0033b3ec5
pmadria <pmadria@mitre.org>
2021-06-23 16:24:12 -0500

    form is now working and now just going to populate the remaining fields and codes

--- db/migrate/20210622174803_create_lab_results.rb
+++ db/migrate/20210622174803_create_lab_results.rb
@@ -1,6 +1,8 @@
 class CreateLabResults < ActiveRecord::Migration[6.1]
   def change
     create_table :lab_results do |t|
+      t.references :patient
+      t.string :json
 
       t.timestamps
     end
