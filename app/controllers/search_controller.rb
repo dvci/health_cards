893efca56961686676f785f7562248a5ccb0f4a3
Shaumik Ashraf <shaumikashraf@mitre.org>
2021-07-02 10:05:20 -0400

    create app/views/search/no_data.html.erb, protected.html.erb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -13,6 +13,7 @@ class SearchController < ApplicationController
   # currently searches local db only
   # will later call QBP client (TODO)
   def query
+    <<-COMMENT
     @matches = Patient.select { |x| x.match?(search_params) }
 
     if @matches.length.zero?
@@ -22,15 +23,44 @@ class SearchController < ApplicationController
     else
       redirect_to @matches.first
     end
+    COMMENT
+
+    results = query(search_params)
+
+    case results[:code]
+    when :AE  # Application Error
+      # TODO
+    when :AR  # Application Rejected
+      raise StandardError.new("QBP client application rejected - QBP client should already have authorization with IIS server")
+    when :NF  # No Data Found
+      render 'no_data.html.erb'
+    when :OK  # Data Found (No Errors)
+      bundle_json = translate(results[:patient])
+      
+    when :PD  # Protected Data
+      render 'protected.html.erb'
+    when :TM  # Too Much Data Found
+      redirect_to search_form_url, alert: "Too many matches found, please enter more information."
+      # is there a case where one patient is found, but there are too many immunizations to load?
+    else
+      raise StandardError.new("QBP client code #{results[:code]} not recognized");
+    end
   end
 
   private
-
   def search_params
     if params[:action] == 'form'
       params[:autofill] ? helpers.real_patient_params[:patient] : {}
     else
       params.require(:patient).permit([:given, :family, :birth_date])
     end
+  end
+
+  def query
+    raise NotImplemented.new("Call QBP Client");
+  end
+
+  def translate
+    raise NotImplemented.new("Call QBP Client");
   end
 end
