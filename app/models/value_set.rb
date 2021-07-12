68881238ffcacbb688a82f2aa38611f0936ed0b5
pmadria <pmadria@mitre.org>
2021-07-12 15:43:21 -0400

    latest changes

--- app/models/value_set.rb
+++ app/models/value_set.rb
@@ -7,9 +7,9 @@ serialize :json, FHIR::ValueSet
 #get the code and display information from the valueset 
 #return list of code and display objects
 def get_info_from_valueset (valueset)
-    valueset_code = valueset.compose.include[0].concept.code
-    display = valueset.compose.include[0].concept.display
-    return [code, display]
+    valueset_code = valueset['compose']['include'][0]['concept'].code
+    display = valueset['compose']['include'][0]['concept'].display
+    return [valueset_code, display]
 end
 
 end
