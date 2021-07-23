040830dd4b8c63f750d922dbbc9fd95b3f855602
Sam Sayer <ssayer@mitre.org>
2021-07-23 13:15:48 -0400

    fixed form and display

--- app/helpers/lab_results_helper.rb
+++ app/helpers/lab_results_helper.rb
@@ -1,2 +1,11 @@
 module LabResultsHelper
+
+  def lab_options(value_set)
+
+    x = value_set.codes.group_by { |code| code.system }.to_a.map do |system|
+      [system.first, system.last.map! { |code| [code.display, code.code] }]
+    end
+
+    grouped_options_for_select(x)
+  end
 end
