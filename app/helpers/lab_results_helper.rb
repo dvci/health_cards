c95a0fcf3efc7238dfe82e3711408b7a675391c6
Stephen MacVicar <smacvicar@mitre.org>
2021-07-27 10:19:16 -0400

    fix edit form

--- app/helpers/lab_results_helper.rb
+++ app/helpers/lab_results_helper.rb
@@ -1,11 +1,8 @@
 module LabResultsHelper
 
   def lab_options(value_set)
-
-    x = value_set.codes.group_by { |code| code.system }.to_a.map do |system|
+    value_set.codes.group_by { |code| code.system }.to_a.map do |system|
       [system.first, system.last.map! { |code| [code.display, code.code] }]
     end
-
-    grouped_options_for_select(x)
   end
 end
