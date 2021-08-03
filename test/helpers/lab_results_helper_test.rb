6f7d221e2e3e1f9da227463834b891cd5a9d6de6
pmadria <pmadria@mitre.org>
2021-08-03 15:08:59 -0500

    slight tweak to test

--- test/helpers/lab_results_helper_test.rb
+++ test/helpers/lab_results_helper_test.rb
@@ -12,5 +12,7 @@ class LabResultsHelperTest < ActiveSupport::TestCase
     value_set = lab_options(example_str)
     assert_not_nil value_set
     assert_equal '10828004', value_set[0][1][0][1]
+    assert_equal 'http://snomed.info/sct', value_set[0][0]
+    assert_equal 'Positive (qualifier value)', value_set[0][1][0][0]
   end
 end
