294898b239e3d4d1dd974076bd3244afa3cd2658
Mick O'Hanlon <mohanlon@mitre.org>
2021-09-13 13:28:42 -0400

    Allow PDF to render in production using Docker (#91)
    * Add build and run docker instructions to README
    
    * Add webpacker commands to Dockerfile
    
    * Remove unused React code causing webpacker to fail
    
    * Add .env.production file
    
    * Update README instructing issuers to pass in their own HOST environment variable
    
    * Remove webpacker:install from Dockerfile
    
    * Removing .env.production until we can add proper testing

--- app/javascript/packs/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from '../components/App'
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(
-    <App/>,
-    document.body.appendChild(document.createElement('div')),
-  )
-})
