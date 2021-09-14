132d76a04b130eefac3d800435f61f6eacf7b4e9
Sam Sayer <ssayer@mitre.org>
2021-09-14 10:02:08 -0400

    add non-US vaccines with tradenames

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,3 +27,16 @@ Vaccine.find_or_create_by(code: '212') do |vaccine|
   vaccine.name = 'Janssen COVID-19 Vaccine'
   vaccine.doses_required = 1
 end
+Vaccine.find_or_create_by(code: '510') do |vaccine|
+  vaccine.name = 'Sinopharm (BIBP) COVID-19 Vaccine'
+  vaccine.doses_required = 2
+end
+Vaccine.find_or_create_by(code: '511') do |vaccine|
+  vaccine.name = 'Coronavac (Sinovac) COVID-19 Vaccine'
+  vaccine.doses_required = 2
+end
+
+Vaccine.find_or_create_by(code: '500') do |vaccine|
+  vaccine.name = 'Coronavac (Sinovac) COVID-19 Vaccine'
+  vaccine.doses_required = 2
+end
