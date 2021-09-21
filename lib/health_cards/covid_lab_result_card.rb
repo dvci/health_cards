f4b5e2204b3998ddb8464ec04569a96124c62c50
mickohanlon23 <mohanlon@mitre.org>
2021-09-21 09:58:05 -0400

    Update allowable attributes within Covid-19 subclasses

--- lib/health_cards/covid_lab_result_card.rb
+++ lib/health_cards/covid_lab_result_card.rb
@@ -4,6 +4,6 @@ module HealthCards
   class COVIDLabResultCard < COVIDHealthCard
     additional_types 'https://smarthealth.cards#laboratory'
 
-    allow type: FHIR::Observation, attributes: %w[status code valueCodeableConcept patient effectiveDateTime]
+    allow type: FHIR::Observation, attributes: %w[meta status code subject patient effectiveDateTime effectiveDatePeriod performer valueCodeableConcept valueQuantity valueString referenceRange]
   end
 end
